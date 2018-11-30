6c64b0663b80eb28a7b1f1cd47112e3aaa8ae51c
Deven <767942678@qq.com>
2018-11-30 18:40:54 +0800

    1.icon预传.2.修改logo.

--- app/src/main/java/com/seaside/ui/MainActivity.java
+++ app/src/main/java/com/seaside/ui/MainActivity.java
@@ -6,9 +6,7 @@ import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TabWidget;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.fth_main)
     FragmentTabHost fthMain;
-    @BindView(android.R.id.tabcontent)
-    FrameLayout tabcontent;
-    @BindView(R.id.realtabcontent)
-    FrameLayout realtabcontent;
-    @BindView(android.R.id.tabs)
-    TabWidget tabs;
+//    @BindView(android.R.id.tabcontent)
+//    FrameLayout tabcontent;
+//    @BindView(R.id.realtabcontent)
+//    FrameLayout realtabcontent;
+//    @BindView(android.R.id.tabs)
+//    TabWidget tabs;
     private String[] tabTag = {"chat", "contacts"};
     private String[] tabName = {"聊天", "联系人"};
     private Class[] fragments = {ChatFragment.class, ContactsFragment.class};
@@ -45,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         int[] rID = new int[]{R.drawable.selector_tab_chat, R.drawable.selector_tab_contacts};
-        fthMain.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        fthMain.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
         fthMain.getTabWidget().setDividerDrawable(null);
         LayoutInflater inflater = LayoutInflater.from(this);
         for (int i = 0; i < tabTag.length; i++) {
